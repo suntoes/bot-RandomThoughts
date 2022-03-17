3023b627e0fdb32633210156d07adcb9756b2244
suntoes <mar26xd@gmail.com>
2022-03-17 14:56:45 +0800

    nav timeout fix attempt #1

--- functions/src/repeater.ts
+++ functions/src/repeater.ts
@@ -7,22 +7,23 @@ let repeatLap = 0;
 const customRepeater = async() => {
     // original function
     repeatLap++;
+    console.log(`init cycle ${repeatLap}...`);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    console.log(`init cycle ${repeatLap}...`);
+    await page.setDefaultNavigationTimeout(0);
     await page.goto(process.env.TWEET_URL);
     // closes the page and browser to avoid any memory error
-    setTimeout(async()=> {
+    await setTimeout(async()=> {
         console.log(`cycle ${repeatLap} done...`);
         await page.close();
         await browser.close();
-    }, 15000)
+    }, 5000)
     // repeats the function completely at n time to avoid lapsing things
-    setTimeout(async()=> {
+    await setTimeout(async()=> {
         await customRepeater();
-    }, 30000)
+    }, 20000)
 }
 
 // initial things
-customRepeater();
 console.log('initializing repeater...');
+customRepeater();
